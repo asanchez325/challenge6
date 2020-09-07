5559a72df47a2a5d14eec783acb9a92dce932dec
Imperial Design <amsanchez@knox.edu>
2020-09-06 21:13:01 -0500

    current day weather section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,51 @@
-//API Key
-var "currentWeather" = 28d7382e0d258bb1a8721bdfe858caf5
 //Elements
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+//API Key
+const APIKey = "28d7382e0d258bb1a8721bdfe858caf5";
+
 //Read users 'search', get current condition from api key
+function getWeather(cityName) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    axios.get(queryURL)
+    .then(function(response){
+        console.log(response);
+
 //Display location current conditions
+const currentDate = new Date(response.data.dt*1000);
+console.log(currentDate);
+
+const day = currentDate.getDate();
+const month = currentDate.getMonth() + 1;
+const year = currentDate.getFullYear();
+nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+let weatherPic = response.data.weather[0].icon;
+currentPicEl.setAttribute("src","placeholder for weather app image" + weatherPic + "@2x.png");
+currentPicEl.setAttribute("alt",response.data.weather[0].description);
+currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+let lat = response.data.coord.lat;
+let lon = response.data.coord.lon;
+let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+axios.get(UVQueryURL)
+.then(function(response){
+let UVIndex = document.createElement("span");
+UVIndex.setAttribute("class","badge badge-danger");
+UVIndex.innerHTML = response.data[0].value;
+currentUVEl.innerHTML = "UV Index: ";
+currentUVEl.append(UVIndex);
+});
+
 //Display 5 day forecast
+})
+initPage();
