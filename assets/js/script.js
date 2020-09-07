438adae392c244690d5647849c99b45eab6039df
Imperial Design <amsanchez@knox.edu>
2020-09-06 20:09:38 -0500

    Add 'search section' and map out project using comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
+//API Key
 var "currentWeather" = 28d7382e0d258bb1a8721bdfe858caf5
+//Elements
+//Read users 'search', get current condition from api key
+//Display location current conditions
+//Display 5 day forecast
